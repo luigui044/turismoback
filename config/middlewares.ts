7472b461d0029c3dbf3b7ee6af6cc039b1ed6000
luigui044 <56895934+luigui044@users.noreply.github.com>
2024-12-01 23:54:45 -0600

    se arregla cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -12,7 +12,7 @@ export default [
     name: 'strapi::cors',
     config: {
       enabled: true,
-      origin: ['http://localhost:5173', 'http://localhost'],
+      origin: ['http://localhost:5173', 'http://localhost', 'tm.probalosv.com', 'turismomas.com'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
     },
