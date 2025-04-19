836fe3da8cdc60489e05f79813fa71427c3afd1d
luigui044 <56895934+luigui044@users.noreply.github.com>
2025-04-18 18:02:41 -0600

    se agrega cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -12,7 +12,7 @@ export default [
     name: 'strapi::cors',
     config: {
       enabled: true,
-      origin: ['http://localhost:5173', 'http://localhost', 'https://probalosv.com', 'https://www.probalosv.com', 'https://tm.probalosv.com', 'https://turismomas.com', 'https://www.turismomas.com', 'https://proto.turismomas.com', 'https://www.turismo.innovasv.com', 'https://turismo.innovasv.com'],
+      origin: ['http://localhost:5173', 'http://localhost', 'https://probalosv.com', 'https://www.probalosv.com', 'https://tm.probalosv.com', 'https://turismomas.com', 'https://www.turismomas.com', 'https://proto.turismomas.com', 'https://www.turismo.innovasv.net', 'https://turismo.innovasv.net'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
     },
